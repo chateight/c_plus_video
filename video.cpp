bdb8ddfd5bc74eba16892dcacd254a7d82579d77
rusami <isehara.jp@coderdojo.com>
2022-11-04 13:38:28 +0900

    #

--- video.cpp
+++ video.cpp
@@ -36,17 +36,4 @@ int main()
     }
 
     return 0;
-
-/*
-// processing video
-    cv::Mat img = cv::imread("sample.jpeg");
-    if(img.empty()) {
-        return -1;
-    }
-    cv::namedWindow("test", cv::WINDOW_AUTOSIZE);
-    cv::imshow("test", img);
-    cv::waitKey(0);
-    cv::destroyWindow("test");
-    return 0;
-*/
 }
